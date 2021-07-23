7325b986d7fac951ae378276a8d3d43c42757b6e
Michal Wowk <michalwowk92@gmail.com>
2021-07-23 12:17:10 +0200

    Normalize styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { css, Global } from "@emotion/react";
+import { Header } from "./componets/Header";
+import normalize from "./styles/normalize";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Global
+        styles={css`
+          ${normalize}
+        `}
+      />
+      <Header />
     </div>
   );
 }
