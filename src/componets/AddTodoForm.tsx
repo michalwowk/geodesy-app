6a42702de42d146d9868a37066ec153d32db7d7a
Michal Wowk <michalwowk92@gmail.com>
2021-07-30 11:25:15 +0200

    Create projects context

---
+++ src/componets/AddTodoForm.tsx
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled/macro";
+import { useFormik } from "formik";
+import { v4 as uuid } from "uuid";
+
+import { colors } from "styles/colors";
+import { VisuallyHidden } from "./VisuallyHidden";
+import { useProjectsContext } from "context/projectsContext";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 20px 0 4px;
+`;
+
+const FormTogglerWrapper = styled.button`
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    color: ${colors.accent};
+    span::after,
+    span::before {
+      background-color: ${colors.accent};
+    }
+  }
+`;
+
+const TogglerButton = styled.span`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  margin-right: 12px;
+  background-color: ${colors.navy};
+  border: none;
+  position: relative;
+  cursor: pointer;
+
+  ::after,
+  ::before {
+    content: "";
+    position: absolute;
+    background-color: ${colors.white};
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  &::before {
+    height: 66%;
+    width: 2px;
+  }
+
+  ::after {
+    width: 66%;
+    height: 2px;
+  }
+`;
+
+const AddTodoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CloseButton = styled.button`
+  background-color: transparent;
+  width: 20px;
+  height: 20px;
+  margin-left: 16px;
+  position: relative;
+  border: none;
+  cursor: pointer;
+  ::before {
+    content: "x";
+    position: absolute;
+    top: 35%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    line-height: 14px;
+    font-size: 20px;
+    color: ${colors.accent};
+  }
+`;
+
+interface Props {
+  columnId: string;
+}
+
+export const AddTodoForm = ({ columnId }: Props) => {
+  const [formState, setFormState] = useState<"closed" | "open">("closed");
+  const { projects, setProjects } = useProjectsContext();
+
+  const formik = useFormik({
+    initialValues: {
+      content: "",
+    },
+    onSubmit: ({ content }) => {
+      const id = uuid();
+      const columns = [...projects.columns];
+
+      const currentColumn = columns.find((column) => column.id === columnId);
+
+      if (!currentColumn) {
+        return;
+      }
+
+      currentColumn.taskIds = [...currentColumn.taskIds, id];
+
+      setProjects({
+        ...projects,
+        columns,
+        tasks: [...projects.tasks, { id, content }],
+      });
+      setFormState("closed");
+    },
+  });
+
+  const handleFormClose = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setFormState("closed");
+  };
+
+  const handleFormOpen = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setFormState("open");
+  };
+
+  const isFormOpen = formState === "open";
+
+  return (
+    <Wrapper>
+      {isFormOpen ? (
+        <form onSubmit={formik.handleSubmit}>
+          <textarea
+            value={formik.values.content}
+            onChange={formik.handleChange}
+            name="content"
+            id="content"
+            placeholder="Dodaj projekt..."
+          />
+          <AddTodoWrapper>
+            <button type="submit">Add project</button>
+            <CloseButton onClick={handleFormClose}>
+              <VisuallyHidden>Close card</VisuallyHidden>
+            </CloseButton>
+          </AddTodoWrapper>
+        </form>
+      ) : (
+        <FormTogglerWrapper onClick={handleFormOpen}>
+          <TogglerButton /> Add project
+        </FormTogglerWrapper>
+      )}
+    </Wrapper>
+  );
+};
