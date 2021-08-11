92bcfbe97acea2f1bf13efd4f9ea49cb9aa78cdb
Michal Wowk <michalwowk92@gmail.com>
2021-08-11 17:01:25 +0200

    Create BoardDataMapper

--- src/utils/initialData.tsx
+++ src/utils/initialData.tsx
@@ -46,3 +46,30 @@ export const BoardData = [
     ],
   },
 ];
+//TODO: Obgadać to z Kubą czy własnie o taki coś mu chodziło gdy mówił o DTO oraz DO. Z tego co czytałem zazwyczaj ma to sens przy pracy
+//      z danymi z API, tutaj póki co zakładam, że to jest moje API.
+
+// Miejsce tych funckji jest przypadkowe, zastanówmy się razem gdzi to powinno się znajdować. Moim zdaniem może być to BoardDataContext lub
+// folder API i plik Mappers
+const findProjectById = (id: string) => {
+  return Projects.find((project) => project.id === id);
+};
+
+export const mapColumnsDataToBoardData = () => {
+  const mappedBoardData = BoardColumnsData.map(
+    ({ id, title, projectsOrder }) => {
+      const projects = projectsOrder.map((projectId) =>
+        findProjectById(projectId)
+      );
+      const newColumns = {
+        id,
+        title,
+        projects,
+      };
+
+      return newColumns;
+    }
+  );
+
+  return mappedBoardData;
+};
